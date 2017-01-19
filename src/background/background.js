cee09480b4bd4d8b39a679df3759707a7ede9bd8
crazy2be <code@jmcgirr.com>
2017-01-19 12:54:10 -0700

    Remove a little bit more fluff.

--- src/background/background.js
+++ src/background/background.js
@@ -1,20 +1,5 @@
-(function() {
-    "use strict";
-
-    /* globals chrome, unescape, keyvalDB, match, matchReplace */
-    var logOnTab = function(tabId, message, important) {
-		important = !!important;
-		chrome.tabs.sendMessage(tabId, {
-			action: "log",
-			message: message,
-			important: important
-		});
-    };
-
-    chrome.webRequest.onBeforeRequest.addListener(function(details) {
-		return {redirectUrl: chrome.extension.getURL("generals-bundle-prod-v11.0.unmin.js")};
-    }, {
-        urls: ["http://generals.io/generals-bundle-prod-v11.0.js"]
-    }, ["blocking"]);
-
-})(); // end script-wide closure
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+	return {redirectUrl: chrome.extension.getURL("generals-bundle-prod-v11.0.unmin.js")};
+}, {
+	urls: ["http://generals.io/generals-bundle-prod-v11.0.js"]
+}, ["blocking"]);
