35a231bde5cda1da214c70693dd930b4b36ec00a
crazy2be <code@jmcgirr.com>
2017-01-27 13:07:10 -0700

    Update for v12.0.2

--- script-replacer.js
+++ script-replacer.js
@@ -1,5 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	return {redirectUrl: chrome.extension.getURL("generals-bundle-prod-v12.0.1.unmin.patched.js")};
+	return {redirectUrl: chrome.extension.getURL("generals-bundle-prod-v12.0.2.unmin.patched.js")};
 }, {
-	urls: ["http://generals.io/generals-bundle-prod-v12.0.1.js"]
+	urls: ["http://generals.io/generals-bundle-prod-v12.0.2.js"]
 }, ["blocking"]);
